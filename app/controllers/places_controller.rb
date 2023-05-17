0c414c6c2166c51414f56911f4c8cbafb1c40063
Chris Basara <chris.basara@kellogg.northwestern.edu>
2023-05-17 19:44:02 +0000

    first updates

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,23 @@
+class PlacesController < ApplicationController
+
+  def index
+    @places = Place.all
+  end
+
+  def new
+    @place = Place.new
+  end
+
+  def create
+    @place = Place.new
+    @place["name"] = params["place"]["name"]
+    @place.save
+  end
+
+  def show
+    @place = Place.find_by(params["id"])
+    @post = Post.where({ "place_id" => @place["id"] })
+  end
+  
+
+end
