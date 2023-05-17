95142aee4e8bb06d55be31bf79812f8ef99afee2
Chris Basara <chris.basara@kellogg.northwestern.edu>
2023-05-17 21:56:03 +0000

    change

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,16 +12,12 @@ class PostsController < ApplicationController
   def create
     @post = Post.new 
   
-  # assign user-entered form data to Post's columns
+  
     @post["title"] = params["post"]["title"]
     @post["description"] = params["post"]["description"]
     @post["posted_on"] = params["post"]["posted_on"]
     @post["place_id"] = params["post"]["place_id"]
-  
-    #save
     @post.save 
-  
-    #redirect user back to home page
     
     redirect_to "/places/#{@post["place_id"]}"
   
